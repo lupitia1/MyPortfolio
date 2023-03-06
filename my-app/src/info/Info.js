8c505a1b662f652eedc7c47ae0691289c86643c3
Patricia <p.alvarenga@diatomea.tech>
2023-03-06 04:08:59 +0100

    modified

--- my-app/src/info/Info.js
+++ my-app/src/info/Info.js
@@ -8,7 +8,8 @@ import mock5 from "../img/mock5.png"
 /* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
 how to make this template work best for you! */
 
-export let colors = ["rgb(0,255,164)", "rgb(166,104,255)"];
+export let colors = ["rgb(0, 247, 178)", "rgb(25, 155, 244)"];
+
 /*
 I highly recommend using a gradient generator like https://gradientgenerator.paytonpierce.dev/ to generate a pair of colors that you like.
 These colors will be used to style your name on the homepage, the background of your picture, and some other accents throughout
@@ -23,9 +24,9 @@ of what each of the values mean.
  */
 
 export const info = {
-    firstName: "John",
-    lastName: "Smith",
-    initials: "js", // the example uses first and last, but feel free to use three or more if you like.
+    firstName: "Patricia",
+    lastName: "Alvarenga",
+    initials: "", // the example uses first and last, but feel free to use three or more if you like.
     position: "a Full Stack Developer",
     selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
     gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
@@ -33,19 +34,19 @@ export const info = {
     miniBio: [ // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
         {
             emoji: '☕',
-            text: 'fueled by coffee'
+            text: 'Fueled by coffee'
         },
         {
             emoji: '🌎',
-            text: 'based in the US'
+            text: 'Based in Spain'
         },
         {
             emoji: "💼",
-            text: "Systems Engineer at Google"
+            text: "Software Analyst at Diatomea Tech"
         },
         {
             emoji: "📧",
-            text: "johnsmith@gmail.com"
+            text: "palvarengam@gmail.com"
         }
     ],
     socials: [
@@ -74,16 +75,16 @@ export const info = {
             icon: "fa fa-twitter",
             label: 'twitter'
         }
-// Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
-// Just change the links so that they lead to your social profiles.
+        // Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
+        // Just change the links so that they lead to your social profiles.
 
     ],
-    bio: "Hello! I'm John. I'm a systems engineer for Google. I studied CompSci at Harvard, I enjoy long walks on the beach, and I believe artificial intelligence will inevitably rule us all one day. You should hire me!",
+    bio: "Hello! I'm Paty. I'm a software analyst at Diatomea Tech. I'm from El Salvador and I went abroad few years ago to complete my Computer Science degree in Spain (where I live now!), I enjoy spending time in nature, and I believe everything is achievable with enough effort and consistence. Let's work together!",
     skills:
-        {
-            proficientWith: ['javascript', 'react', 'git', 'github', 'bootstrap', 'html5', 'css3', 'figma'],
-            exposedTo: ['nodejs', 'python', 'adobe illustrator']
-        }
+    {
+        proficientWith: ['Node', 'Python', 'Typescript', 'Git versioning','API/REST', 'Agile methodologies', 'AWS', 'React', 'SQL', 'Docker', 'Bash/Scripting', 'Linux Systems'],
+        exposedTo: ['Java', 'NoSQL', 'Kubernetes', 'C#', 'MQTT', 'Testing frameworks']
+    }
     ,
     hobbies: [
         {
@@ -91,19 +92,19 @@ export const info = {
             emoji: '📖'
         },
         {
-            label: 'theater',
-            emoji: '🎭'
+            label: 'bouldering',
+            emoji: '🧗'
         },
         {
-            label: 'movies',
-            emoji: '🎥'
+            label: 'parties',
+            emoji: '🥳'
         },
         {
-            label: 'cooking',
-            emoji: '🌶'
+            label: 'traveling',
+            emoji: '🎒'
         }
-// Same as above, change the emojis to match / relate to your hobbies or interests.
-// You can also remove the emojis if you'd like, I just think they look cute :P
+        // Same as above, change the emojis to match / relate to your hobbies or interests.
+        // You can also remove the emojis if you'd like, I just think they look cute :P
     ],
     portfolio: [ // This is where your portfolio projects will be detailed
         {
